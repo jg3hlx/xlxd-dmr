ccc3386e32036944db8db2a15cdf3d676262c8c8
JG3HLX <76939165+jg3hlx@users.noreply.github.com>
2026-05-11 18:24:30 +0900

    Update suffix  cases in repeaters.php

--- dashboard2/pgs/repeaters.php
+++ dashboard2/pgs/repeaters.php
@@ -44,13 +44,14 @@ for ($i=0;$i<$Reflector->NodeCount();$i++) {
    }
    else {
       switch ($Reflector->Nodes[$i]->GetSuffix()) {
-         case 'A' : echo '23cm'; break;
-         case 'B' : echo '70cm'; break;
-         case 'C' : echo '2m'; break;
-         case 'D' : echo 'Dongle'; break;
-         case 'G' : echo 'Internet-Gateway'; break;
-         default  : echo '';
-      }
+            case 'A' : echo '1200GHz'; break;
+            case 'B' : echo '430MHz'; break;
+            case 'C' : echo '144MHz'; break;
+            case 'D' : echo 'Dongle'; break;
+            case 'G' : echo 'Internet-Gateway'; break;
+            case 'M' : echo $Reflector->Nodes[$i]->GetDMRID(); break;
+            case 'P' : echo 'Pi-Star-NODE'; break;
+            default  : echo ''      }
    }
    echo '</td>
    <td>'.date("d.m.Y H:i", $Reflector->Nodes[$i]->GetLastHeardTime()).'</td>
